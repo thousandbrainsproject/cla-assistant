e5909139c11bf682e6377d51ffc16b3b964ae3ac
Tristan Slominski <tslominski@numenta.com>
2024-10-18 11:45:26 -0500

    debug: debugging permissions and commenting on pr

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,10 @@
 import * as core from "@actions/core";
 import { Octokit } from "octokit";
 
+// TODO: Remove artisanal versioning after debugging
 console.log("2024-10-18 10:22");
+
+const CLA_LINK = "https://na4.documents.adobe.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhA-C5ccSQcDGY-PiamH4HnZdj5p2I1oDc8FiBJ_23pReFeauFhfcIkC1XfzxC2qnBQ*";
 
 // TODO: Update after renaming TBP_BOT_PAT to TBP_BOT_TOKEN_SECRET in nupic.monty
 const TBP_BOT_TOKEN_SECRET = process.env.TBP_BOT_PAT;
@@ -20,33 +23,50 @@ if (!TBP_BOT_TOKEN_SECRET)
     process.exit(1);
 }
 
-const octokit = new Octokit(
+const tbpBotOctokit = new Octokit(
     {
         auth: TBP_BOT_TOKEN_SECRET
     }
 );
 
-const claSignatories = await octokit.paginate(
-    octokit.rest.teams.listMembersInOrg,
+const claSignatories = await tbpBotOctokit.paginate(
+    tbpBotOctokit.rest.teams.listMembersInOrg,
     {
         org: "numenta",
         team_slug: "nupic-contrib"
     }
 );
 
-const pullRequestAuthor = core.getInput("pull-request-author") || core.getInput("who-to-greet");
+const prAuthor = core.getInput("pull-request-author") || core.getInput("who-to-greet");
+const prNumber = parseInt(core.getInput("pull-request-number")) || 463;
 
-const prAuthorCLASignatory = claSignatories.find(signatory => signatory.login == pullRequestAuthor);
+const prAuthorCLASignatory = claSignatories.find(signatory => signatory.login == prAuthor);
 
-if (!prAuthorCLASignatory)
+if (prAuthorCLASignatory)
 {
-    core.setFailed(`${pullRequestAuthor} has not signed the CLA.`);
-    process.exit(1);
+    console.log(`${prAuthor} has signed the CLA.`);
+    console.log("Proceeding for debugging reasons...");
+    // process.exit(0);
 }
-else
-{
-    console.log(`${pullRequestAuthor} has signed the CLA.`);
-}
+
+core.setFailed(`${prAuthor} has not signed the CLA.`);
+
+const prOctokit = new Octokit(
+    {
+        auth: process.env.GITHUB_TOKEN
+    }
+);
+
+await prOctokit.rest.issues.createComment(
+    {
+        owner: "numentacorp",
+        repo: "nupic.monty",
+        issue_number: prNumber,
+        body: `Thank you for your contribution! It appears that you haven't signed our Contributor License Agreement (CLA) yet. Please [visit this link and sign](${CLA_LINK}).`
+    }
+);
+console.log("Comment with CLA link posted on the pull request.");
+process.exit(1);
 
 try
 {
